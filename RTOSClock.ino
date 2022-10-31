e62736d2a10bd17ea2f355e446f5995b17b81d30
Exojets <RyPK94@gmail.com>
2022-10-30 19:44:58 -0700

    Added comments

--- RTOSClock.ino
+++ RTOSClock.ino
@@ -1,26 +1,31 @@
-#include "RTOSClock.h"
+//Author: Ryan Kelly
+
+#include "RTOSClock.h" //Function prototypes
 #include <LiquidCrystal_I2C.h>
 
-#define timePin 5
-#define hourPin 18
-#define minutePin 19
-#define alarmPin 16
-#define snoozePin 17
-#define switchPin 21
-#define buzzerPin 27
-#define lightPin 33
+// GPIO Pins
+static const int timePin = 5, hourPin = 18, minutePin = 19, alarmPin = 16, snoozePin = 17, switchPin = 21, buzzerPin = 27, lightPin = 33;
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+// Globals
+LiquidCrystal_I2C lcd(0x27, 16, 2); // L2C LCD screen at address 0x27 with 16 columns and 2 rows
 
-static const uint16_t timer_divider = 80;
-static const uint64_t timer_max = 1000000;
+static const uint16_t timer_divider = 80; // Timer ticks at 1 MHz
+static const uint64_t timer_max = 1000000; // Timer reaches max once one second has elapsed 
 static hw_timer_t* timer = NULL;
-SemaphoreHandle_t xSoundSemaphore1, xSoundSemaphore2, xSoundSemaphore3, xLightSemaphore1, xLightSemaphore2, xLightSemaphore3;
+
+SemaphoreHandle_t xSoundSemaphore1, xSoundSemaphore3, xSoundSemaphore2, xLightSemaphore1, xLightSemaphore3, xLightSemaphore2;
+
 static TaskHandle_t timer_init_task = NULL, interrupt_init_task = NULL;
+
 uint16_t hour = 12, minute = 0, second = 0, meridiem = 0, alarm_hour = 12, alarm_minute = 0, alarm_meridiem = 0;
 unsigned long button_time = 0, last_button_time = 0;
 bool alarm_active, alarm_select = false;
 
+//****************************************************************************************************************************************
+// Functions
+
+// Having timer initialization be in its own task allows the timer to be pinned to a specific CPU core. In this program the
+// timer is pinned to the first core.
 void timerInit(void* parameter){
   timer = timerBegin(0, timer_divider, true);
 
@@ -35,154 +40,58 @@ void timerInit(void* parameter){
   }
 }
 
+// Having the initial hardware interrupts be in their own task allows these interrupts and all future interrupts that result
+// from them to be pinned to a specific CPU core. In this program all interrupts are pinned to the second core.
 void interruptInit(void* parameter){
   attachInterrupt(digitalPinToInterrupt(timePin), timeButtonHeld, RISING);
   attachInterrupt(digitalPinToInterrupt(alarmPin), alarmButtonHeld, RISING);
 
-  while(1);
+  while(1){
+    
+  }
 }
 
-void IRAM_ATTR onTimer(){
-  if(second < 59)
-    second++;
-  else if(minute < 59){
-    minute++;
-    second = 0;
-  }
-  else{
-    if(hour == 11){
-      meridiem ^= 1;
-      hour++;
+// Produces a repeating beeping noise from a buzzer when activated via the binary semaphore xSoundSemaphore1 (given by the
+// task alarmCheck), is paused for 5 minutes via the binary semaphore xSoundSemaphore2 (given by the ISR snooze), is
+// deactivated via the binary semaphore xSoundSemaphore3 (given by the ISR alarmSwitchOff).
+void alarmSound(void* parameter){
+  while(1){
+    xSemaphoreTake(xSoundSemaphore1, portMAX_DELAY);
+    while(1){
+      if(xSemaphoreTake(xSoundSemaphore3, 0) == pdTRUE)
+        break;
+      tone(buzzerPin, 800, 300);
+      vTaskDelay(600 / portTICK_PERIOD_MS);
+      if(xSemaphoreTake(xSoundSemaphore2, 0) == pdTRUE)
+        if(xSemaphoreTake(xSoundSemaphore3, (300000 / portTICK_PERIOD_MS)) == pdTRUE)
+          break;
     }
-    else if(hour == 12)
-      hour = 1;
-    else
-      hour++;
-    minute = 0;
-    second = 0;
   }
 }
 
-void IRAM_ATTR timeButtonHeld(){
-  button_time = millis();
-  if (button_time - last_button_time > 250)
-  {
-    detachInterrupt(digitalPinToInterrupt(timePin));
-    attachInterrupt(digitalPinToInterrupt(timePin), timeButtonReleased, FALLING);
-    attachInterrupt(digitalPinToInterrupt(hourPin), hourButtonPressedTime, RISING);
-    attachInterrupt(digitalPinToInterrupt(minutePin), minuteButtonPressedTime, RISING);
-    detachInterrupt(digitalPinToInterrupt(alarmPin));
-    last_button_time = button_time;
-  }
-}
-
-void IRAM_ATTR timeButtonReleased(){
-  button_time = millis();
-  if (button_time - last_button_time > 250)
-  {
-    detachInterrupt(digitalPinToInterrupt(timePin));
-    attachInterrupt(digitalPinToInterrupt(timePin), timeButtonHeld, RISING);
-    detachInterrupt(digitalPinToInterrupt(hourPin));
-    detachInterrupt(digitalPinToInterrupt(minutePin));
-    attachInterrupt(digitalPinToInterrupt(alarmPin), alarmButtonHeld, RISING);
-    if(!timerAlarmEnabled(timer))
-      timerAlarmEnable(timer);
-    last_button_time = button_time;
-  }
-}
-
-void IRAM_ATTR hourButtonPressedTime(){
-  button_time = millis();
-  if (button_time - last_button_time > 250)
-  {
-    if(timerAlarmEnabled(timer)){
-      second = 0;
-      timerAlarmDisable(timer);
+// Repeatedly turns an LED on and off when activated via the binary semaphore xLightSemaphore1 (given by the task alarmCheck),
+// is paused for 5 minutes via the binary semaphore xLightSemaphore2 (given by the ISR snooze), is deactivated via the binary
+// semaphore xLightSemaphore3 (given by the ISR alarmSwitchOff).
+void alarmLight(void* parameter){
+  while(1){
+    xSemaphoreTake(xLightSemaphore1, portMAX_DELAY);
+    while(1){
+      if(xSemaphoreTake(xLightSemaphore3, 0) == pdTRUE)
+        break;
+      digitalWrite(lightPin, HIGH);
+      vTaskDelay(100 / portTICK_PERIOD_MS);
+      digitalWrite(lightPin, LOW);
+      vTaskDelay(100 / portTICK_PERIOD_MS);
+      if(xSemaphoreTake(xLightSemaphore2, 0) == pdTRUE)
+        if(xSemaphoreTake(xLightSemaphore3, (300000 / portTICK_PERIOD_MS)) == pdTRUE)
+          break;
     }
-    if(hour == 11){
-      meridiem ^= 1;
-      hour++;
-    }
-    else if(hour == 12)
-      hour = 1;
-    else
-      hour++;
-    last_button_time = button_time;
   }
 }
 
-void IRAM_ATTR minuteButtonPressedTime(){
-  button_time = millis();
-  if (button_time - last_button_time > 250)
-  {
-    if(timerAlarmEnabled(timer)){
-      second = 0;
-      timerAlarmDisable(timer);
-    }
-    if(minute < 59)
-      minute++;
-    else
-      minute = 0;
-    last_button_time = button_time;
-  }
-}
-
-void IRAM_ATTR alarmButtonHeld(){
-  button_time = millis();
-  if (button_time - last_button_time > 250)
-  {
-    alarm_select = true;
-    detachInterrupt(digitalPinToInterrupt(alarmPin));
-    attachInterrupt(digitalPinToInterrupt(alarmPin), alarmButtonReleased, FALLING);
-    attachInterrupt(digitalPinToInterrupt(hourPin), hourButtonPressedAlarm, RISING);
-    attachInterrupt(digitalPinToInterrupt(minutePin), minuteButtonPressedAlarm, RISING);
-    detachInterrupt(digitalPinToInterrupt(timePin));
-    last_button_time = button_time;
-  }
-}
-
-void IRAM_ATTR alarmButtonReleased(){
-  button_time = millis();
-  if (button_time - last_button_time > 250)
-  {
-    alarm_select = false;
-    detachInterrupt(digitalPinToInterrupt(alarmPin));
-    attachInterrupt(digitalPinToInterrupt(alarmPin), alarmButtonHeld, RISING);
-    detachInterrupt(digitalPinToInterrupt(hourPin));
-    detachInterrupt(digitalPinToInterrupt(minutePin));
-    attachInterrupt(digitalPinToInterrupt(timePin), timeButtonHeld, RISING);
-    last_button_time = button_time;
-  }
-}
-
-void IRAM_ATTR hourButtonPressedAlarm(){
-  button_time = millis();
-  if (button_time - last_button_time > 250)
-  {
-    if(alarm_hour == 11){
-      alarm_meridiem ^= 1;
-      alarm_hour++;
-    }
-    else if(alarm_hour == 12)
-      alarm_hour = 1;
-    else
-      alarm_hour++;
-    last_button_time = button_time;
-  }
-}
-
-void IRAM_ATTR minuteButtonPressedAlarm(){
-  button_time = millis();
-  if (button_time - last_button_time > 250)
-  {
-    if(alarm_minute < 59)
-      alarm_minute++;
-    else
-      alarm_minute = 0;
-    last_button_time = button_time;
-  }
-}
-
+// Checks to see if the alarm switch is in the "On" position and if the clock's time matches the time set for the alarm.
+// If both are true, gives binary semaphores needed to activate the nested loops found in the alarmSound and alarmLight
+// tasks, thereby causing the buzzer to buzz and the LED to blink.
 void alarmCheck(void* parameter){
   while(1){
     if(digitalRead(switchPin) == HIGH && hour == alarm_hour && minute == alarm_minute && second == 0 && !alarm_active){
@@ -196,65 +105,11 @@ void alarmCheck(void* parameter){
   }
 }
 
-void alarmSound(void* parameter){
-  while(1){
-    xSemaphoreTake(xSoundSemaphore1, portMAX_DELAY);
-    while(1){
-      if(xSemaphoreTake(xSoundSemaphore2, 0) == pdTRUE)
-        break;
-      tone(buzzerPin, 800, 300);
-      vTaskDelay(600 / portTICK_PERIOD_MS);
-      if(xSemaphoreTake(xSoundSemaphore3, 0) == pdTRUE)
-        if(xSemaphoreTake(xSoundSemaphore2, (300000 / portTICK_PERIOD_MS)) == pdTRUE)
-          break;
-    }
-  }
-}
-
-void alarmLight(void* parameter){
-  while(1){
-    xSemaphoreTake(xLightSemaphore1, portMAX_DELAY);
-    while(1){
-      if(xSemaphoreTake(xLightSemaphore2, 0) == pdTRUE)
-        break;
-      digitalWrite(lightPin, HIGH);
-      vTaskDelay(100 / portTICK_PERIOD_MS);
-      digitalWrite(lightPin, LOW);
-      vTaskDelay(100 / portTICK_PERIOD_MS);
-      if(xSemaphoreTake(xLightSemaphore3, 0) == pdTRUE)
-        if(xSemaphoreTake(xLightSemaphore2, (300000 / portTICK_PERIOD_MS)) == pdTRUE)
-          break;
-    }
-  }
-}
-
-void IRAM_ATTR alarmSwitchOff(){
-  button_time = millis();
-  if (button_time - last_button_time > 250)
-  {
-    alarm_active = false;
-    xSemaphoreGive(xSoundSemaphore2);
-    xSemaphoreGive(xLightSemaphore2);
-    detachInterrupt(digitalPinToInterrupt(switchPin));
-    detachInterrupt(digitalPinToInterrupt(snoozePin));
-    last_button_time = button_time;
-  }
-}
-
-void IRAM_ATTR snooze(){
-  button_time = millis();
-  if (button_time - last_button_time > 250)
-  {
-    xSemaphoreGive(xSoundSemaphore3);
-    xSemaphoreGive(xLightSemaphore3);
-    last_button_time = button_time;
-  }
-}
-
+// Updates the time displayed on the LCD screen.
 void draw(void* parameter){
   String time, hour_string, minute_string, second_string, meridiem_string;
   while(1){
-    if(alarm_select == false){
+    if(alarm_select == false){ // If the alarm button is held down, the LCD screen will display the time being set for the alarm
       if(hour < 10)
         hour_string = String(" " + String(hour));
       else
@@ -293,27 +148,210 @@ void draw(void* parameter){
   }
 }
 
+//****************************************************************************************************************************************
+// Interrupt Service Routines (ISRs)
+
+// Increments the time displayed on the clock by one second, executes when timer reaches max (and resets).
+void IRAM_ATTR onTimer(){
+  if(second < 59)
+    second++;
+  else if(minute < 59){
+    minute++;
+    second = 0;
+  }
+  else{
+    if(hour == 11){
+      meridiem ^= 1;
+      hour++;
+    }
+    else if(hour == 12)
+      hour = 1;
+    else
+      hour++;
+    minute = 0;
+    second = 0;
+  }
+}
+
+// This ISR is initially attached by the task interruptInit, and can later be reattached by the ISR timeButtonReleased.
+void IRAM_ATTR timeButtonHeld(){
+  button_time = millis();
+  if (button_time - last_button_time > 250) // If statement "debounces" the button to ensure that the code in the ISR only executes once for one button press
+  {
+    detachInterrupt(digitalPinToInterrupt(timePin));
+    attachInterrupt(digitalPinToInterrupt(timePin), timeButtonReleased, FALLING);
+    attachInterrupt(digitalPinToInterrupt(hourPin), hourButtonPressedTime, RISING);
+    attachInterrupt(digitalPinToInterrupt(minutePin), minuteButtonPressedTime, RISING);
+    detachInterrupt(digitalPinToInterrupt(alarmPin)); // Prevents the alarm button from being used while the time button is held down
+    last_button_time = button_time;
+  }
+}
+
+// This ISR is attached by the ISR timeButtonHeld.
+void IRAM_ATTR timeButtonReleased(){
+  button_time = millis();
+  if (button_time - last_button_time > 250)
+  {
+    detachInterrupt(digitalPinToInterrupt(timePin));
+    attachInterrupt(digitalPinToInterrupt(timePin), timeButtonHeld, RISING);
+    detachInterrupt(digitalPinToInterrupt(hourPin));
+    detachInterrupt(digitalPinToInterrupt(minutePin));
+    attachInterrupt(digitalPinToInterrupt(alarmPin), alarmButtonHeld, RISING); // The alarm button can once again be used now that the time button is no longer held down
+    if(!timerAlarmEnabled(timer))
+      timerAlarmEnable(timer);
+    last_button_time = button_time;
+  }
+}
+
+// This ISR is attached by the ISR timeButtonHeld, and increments the hour while the time is being set.
+void IRAM_ATTR hourButtonPressedTime(){
+  button_time = millis();
+  if (button_time - last_button_time > 250)
+  {
+    if(timerAlarmEnabled(timer)){ // Set the second to 0 and stop the clock from ticking while time is being set
+      second = 0;
+      timerAlarmDisable(timer);
+    }
+    if(hour == 11){
+      meridiem ^= 1;
+      hour++;
+    }
+    else if(hour == 12)
+      hour = 1;
+    else
+      hour++;
+    last_button_time = button_time;
+  }
+}
+
+// This ISR is attached by the ISR timeButtonHeld, and increments the minute while the time is being set.
+void IRAM_ATTR minuteButtonPressedTime(){
+  button_time = millis();
+  if (button_time - last_button_time > 250)
+  {
+    if(timerAlarmEnabled(timer)){ // Set the second to 0 and stop the clock from ticking while time is being set
+      second = 0;
+      timerAlarmDisable(timer);
+    }
+    if(minute < 59)
+      minute++;
+    else
+      minute = 0;
+    last_button_time = button_time;
+  }
+}
+
+// This ISR is initially attached by the task interruptInit, and can later be reattached by the ISR alarmButtonReleased.
+void IRAM_ATTR alarmButtonHeld(){
+  button_time = millis();
+  if (button_time - last_button_time > 250)
+  {
+    alarm_select = true;
+    detachInterrupt(digitalPinToInterrupt(alarmPin));
+    attachInterrupt(digitalPinToInterrupt(alarmPin), alarmButtonReleased, FALLING);
+    attachInterrupt(digitalPinToInterrupt(hourPin), hourButtonPressedAlarm, RISING);
+    attachInterrupt(digitalPinToInterrupt(minutePin), minuteButtonPressedAlarm, RISING);
+    detachInterrupt(digitalPinToInterrupt(timePin)); // Prevents the time button from being used while the alarm button is held down
+    last_button_time = button_time;
+  }
+}
+
+// This ISR is attached by the ISR alarmButtonHeld.
+void IRAM_ATTR alarmButtonReleased(){
+  button_time = millis();
+  if (button_time - last_button_time > 250)
+  {
+    alarm_select = false;
+    detachInterrupt(digitalPinToInterrupt(alarmPin));
+    attachInterrupt(digitalPinToInterrupt(alarmPin), alarmButtonHeld, RISING);
+    detachInterrupt(digitalPinToInterrupt(hourPin));
+    detachInterrupt(digitalPinToInterrupt(minutePin));
+    attachInterrupt(digitalPinToInterrupt(timePin), timeButtonHeld, RISING); // The time button can once again be used now that the alarm button is no longer held down
+    last_button_time = button_time;
+  }
+}
+
+// This ISR is attached by the ISR alarmButtonHeld, and increments the hour while the alarm is being set.
+void IRAM_ATTR hourButtonPressedAlarm(){
+  button_time = millis();
+  if (button_time - last_button_time > 250)
+  {
+    if(alarm_hour == 11){
+      alarm_meridiem ^= 1;
+      alarm_hour++;
+    }
+    else if(alarm_hour == 12)
+      alarm_hour = 1;
+    else
+      alarm_hour++;
+    last_button_time = button_time;
+  }
+}
+
+// This ISR is attached by the ISR alarmButtonHeld, and increments the minute while the alarm is being set.
+void IRAM_ATTR minuteButtonPressedAlarm(){
+  button_time = millis();
+  if (button_time - last_button_time > 250)
+  {
+    if(alarm_minute < 59)
+      alarm_minute++;
+    else
+      alarm_minute = 0;
+    last_button_time = button_time;
+  }
+}
+
+// This ISR is attached by the ISR alarmCheck, and gives the binary semaphores needed to break the nested loops found in the
+// alarmSound and alarmLight tasks, thereby stopping the buzzer from buzzing and the LED from blinking.
+void IRAM_ATTR alarmSwitchOff(){
+  button_time = millis();
+  if (button_time - last_button_time > 250)
+  {
+    alarm_active = false;
+    xSemaphoreGive(xSoundSemaphore3);
+    xSemaphoreGive(xLightSemaphore3);
+    detachInterrupt(digitalPinToInterrupt(switchPin));
+    detachInterrupt(digitalPinToInterrupt(snoozePin));
+    last_button_time = button_time;
+  }
+}
+
+// This ISR is attached by the ISR alarmCheck, and gives the binary semaphores needed to make the alarmSound and alarmLight
+// tasks wait for 5 minutes, thereby temporarily stopping the buzzer from buzzing and the LED from blinking.
+void IRAM_ATTR snooze(){
+  button_time = millis();
+  if (button_time - last_button_time > 250)
+  {
+    xSemaphoreGive(xSoundSemaphore2);
+    xSemaphoreGive(xLightSemaphore2);
+    last_button_time = button_time;
+  }
+}
+
+//****************************************************************************************************************************************
+// Main
+
 void setup() {
-  pinMode(5, INPUT); //Time
-  pinMode(18, INPUT); //Hour
-  pinMode(19, INPUT); //Minute
-  pinMode(16, INPUT); //Alarm
-  pinMode(21, INPUT); //Switch
-  pinMode(17, INPUT); //Snooze
-  pinMode(27, OUTPUT); //Buzzer
-  pinMode(33, OUTPUT); //Light
+  pinMode(timePin, INPUT);
+  pinMode(hourPin, INPUT);
+  pinMode(minutePin, INPUT);
+  pinMode(alarmPin, INPUT);
+  pinMode(switchPin, INPUT);
+  pinMode(snoozePin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(lightPin, OUTPUT);
 
   Serial.begin(115200);
 
   lcd.init();                      
-  lcd.backlight();
+  lcd.backlight(); // Turns on the LCD screen's backlight
 
   xSoundSemaphore1 = xSemaphoreCreateBinary();
-  xSoundSemaphore2 = xSemaphoreCreateBinary();
   xSoundSemaphore3 = xSemaphoreCreateBinary();
+  xSoundSemaphore2 = xSemaphoreCreateBinary();
   xLightSemaphore1 = xSemaphoreCreateBinary();
-  xLightSemaphore2 = xSemaphoreCreateBinary();
   xLightSemaphore3 = xSemaphoreCreateBinary();
+  xLightSemaphore2 = xSemaphoreCreateBinary();
   
   xTaskCreatePinnedToCore(
                           timerInit,
@@ -368,12 +406,13 @@ void setup() {
                           1,
                           NULL,
                           1);
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  vTaskDelay(1000 / portTICK_PERIOD_MS); // Give timerInit and interruptInit tasks enough time to execute before deleting them
   vTaskDelete(timer_init_task);
   vTaskDelete(interrupt_init_task);
-  vTaskDelete(NULL);
+  vTaskDelete(NULL); // Delete setup and loop tasks
 }
 
+// Left empty, will be deleted at the end of setup
 void loop() {
   
 }
